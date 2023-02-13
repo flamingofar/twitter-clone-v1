d5525c38e8676327c862151bd41805bfcfc9c20c
malte <skjoldager1066@gmail.com>
2023-02-13 16:37:29 +0100

    test

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ import os
 import g
 
 #*############################# CONNECTS GITHUB AND PYTHONANYWHERE
-
-# https://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@github.com/flamingofar/twitter-clone-v1.git
-
 @post('/13e4520155514c39be45e5d5cdac559c')
 def git_update():
   repo = git.Repo('./twitter-clone-v1')
